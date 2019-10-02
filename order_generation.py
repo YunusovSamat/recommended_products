8ac4e5d1a57b25102955202309aa5952c9c3c2aa
YunusovSamat <samat.yunusov@bk.ru>
2019-10-02 13:13:51 +0300

    Added exception checking and private attributes

--- order_generation.py
+++ order_generation.py
@@ -1,31 +1,40 @@
+import os
 import random
 
 
 class OrderGeneration:
     def __init__(self):
-        self._products = [
+        self.__products = (
             'footwear', 'trousers', 'a cap', 'jacket', 'T-shirt',
             'coat', 'hoody', 'sweater', 'jeans', 'shorts',
             'sneakers', 'dress', 'a bag', 'belt', 'backpack'
-        ]
-        self._len_products = len(self._products)
-        self._orders = list()
+        )
+        self.__len_products = len(self.__products)
+        self.__orders = list()
 
     def orders_generation(self) -> None:
-        self._orders.clear()
+        self.__orders.clear()
         for i in range(random.randint(1, 10)):
-            row_orders = ';'.join(
-                random.sample(self._products, random.randint(1, 5))
+            line = ';'.join(
+                random.sample(self.__products, random.randint(1, 5))
             ) + ';'
-            self._orders.append(row_orders)
+            self.__orders.append(line)
 
-    def write_orders_to_file(self, file_path: str) -> None:
-        with open(file_path, 'w') as file:
-            file.write('\n'.join(self._orders))
+    def write_to_file(self, file_path: str) -> None:
+        file_dirs, file_name = os.path.split(file_path)
+        if os.path.isdir(file_dirs):
+            if file_name:
+                with open(file_path, 'w') as file:
+                    file.write('\n'.join(self.__orders))
+                print("Record to file")
+            else:
+                print("Error: object is a directory")
+        else:
+            print("Error: No such file or directory")
 
 
 if __name__ == '__main__':
     gen = OrderGeneration()
     gen.orders_generation()
-    gen.write_orders_to_file(r'/home/samat/PycharmProjects/TaskPackage/'
-                             r'recommended_products/orders02')
+    gen.write_to_file(r'/home/samat/PycharmProjects/TaskPackage/'
+                      r'recommended_products/orders01.txt')
